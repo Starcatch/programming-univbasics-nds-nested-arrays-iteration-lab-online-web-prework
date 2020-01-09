58249d30e3b0d954fa6a41ff7b07c61c16becc5e
Timur Mishiev <timurmishiev@gmail.com>
2020-01-09 21:31:43 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -38,7 +38,9 @@ def total_even_pairs(src)
   # the number was even. Review the operator documentation if you've forgotten
   # this!
   
-  
-  
+  total = 0
+  row_index = 0
+  while row_index < scr.count do
+    if scr
   
 end
